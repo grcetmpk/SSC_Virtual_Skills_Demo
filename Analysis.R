39e6de6e738600f1c5e7bd6e1581f023e8d78850
grcetmpk <grace.tompkins97@gmail.com>
2026-01-19 11:26:04 -0800

    add analysis code

--- Analysis.R
+++ Analysis.R
@@ -1,1 +1,8 @@
-# This is an R Script for my project
+#--- This is an R Script for my project
+
+# look at first few rows of data set
+head(mtcars)
+
+# fit model of relationship between wt and mpg
+model <- lm(mpg ~ wt, data = mtcars)
+summary(model)
